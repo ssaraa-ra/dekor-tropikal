e63d9fe46b51585b3150b6886e2c55b0e519b3bd
Sara <136077116+ssaraa-ra@users.noreply.github.com>
2025-05-08 21:24:42 +0200

    Add files via upload

--- Dekortropikal/assets/js/script.js
+++ Dekortropikal/assets/js/script.js
@@ -1,9 +1,8 @@
-'use strict';
+"use strict";
 
 /**
  * navbar toggle
  */
-
 const overlay = document.querySelector("[data-overlay]");
 const navOpenBtn = document.querySelector("[data-nav-open-btn]");
 const navbar = document.querySelector("[data-navbar]");
@@ -19,12 +18,10 @@ const navToggleEvent = function (elem) {
       overlay.classList.toggle("active");
     });
   }
-}
+};
 
 navToggleEvent(navElemArr);
 navToggleEvent(navLinks);
-
-
 
 /**
  * header sticky & go to top
@@ -34,7 +31,6 @@ const header = document.querySelector("[data-header]");
 const goTopBtn = document.querySelector("[data-go-top]");
 
 window.addEventListener("scroll", function () {
-
   if (window.scrollY >= 200) {
     header.classList.add("active");
     goTopBtn.classList.add("active");
@@ -42,63 +38,49 @@ window.addEventListener("scroll", function () {
     header.classList.remove("active");
     goTopBtn.classList.remove("active");
   }
-
 });
 
-
-document.querySelectorAll('.question').forEach(item => {
-  item.addEventListener('click', () => {
+document.querySelectorAll(".question").forEach((item) => {
+  item.addEventListener("click", () => {
     const parent = item.parentElement;
-    parent.classList.toggle('active');
+    parent.classList.toggle("active");
   });
 });
-
-
-
-
-
-
-
-
 
 function toggleAnswer(question) {
-  const faqItem = question.closest('.faq-item');
-  faqItem.classList.toggle('open');
+  const faqItem = question.closest(".faq-item");
+  faqItem.classList.toggle("open");
 }
 
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function() {
+$(document).ready(function () {
   $("[unique-script-id='w-w-dm-id'] .img .desc").hide();
 
-  $("[unique-script-id='w-w-dm-id'] .img").mouseenter(function(item) {
-    $("[unique-script-id='w-w-dm-id'] .img .overlay").removeClass("overlay-visible");
+  $("[unique-script-id='w-w-dm-id'] .img").mouseenter(function (item) {
+    $("[unique-script-id='w-w-dm-id'] .img .overlay").removeClass(
+      "overlay-visible"
+    );
     $("[unique-script-id='w-w-dm-id'] .img .desc").hide();
-    $("#" + $(item.currentTarget).attr("id") + ' .overlay').addClass("overlay-visible");
-    $("#" + $(item.currentTarget).attr("id") + ' .desc').show();
+    $("#" + $(item.currentTarget).attr("id") + " .overlay").addClass(
+      "overlay-visible"
+    );
+    $("#" + $(item.currentTarget).attr("id") + " .desc").show();
     console.log(item.currentTarget);
   });
-  $("[unique-script-id='w-w-dm-id'] .tab").click(function() {
-    const value = $(this).attr('data-filter');
-    if (value == 'all') {
-      $("[unique-script-id='w-w-dm-id'] .img").show('5000');
+  $("[unique-script-id='w-w-dm-id'] .tab").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $("[unique-script-id='w-w-dm-id'] .img").show("5000");
     } else {
-      $("[unique-script-id='w-w-dm-id'] .img").not('.' + value).hide('5000');
-      $("[unique-script-id='w-w-dm-id'] .img").filter('.' + value).show('5000');
+      $("[unique-script-id='w-w-dm-id'] .img")
+        .not("." + value)
+        .hide("5000");
+      $("[unique-script-id='w-w-dm-id'] .img")
+        .filter("." + value)
+        .show("5000");
     }
   });
 
-  $("[unique-script-id='w-w-dm-id'] .tab").click(function() {
-    $(this).addClass('tab-active').siblings().removeClass('tab-active');
+  $("[unique-script-id='w-w-dm-id'] .tab").click(function () {
+    $(this).addClass("tab-active").siblings().removeClass("tab-active");
   });
-})
-
-
+});
